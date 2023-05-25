9b28134bee4066e1142aa534482dfcc0b923312d
Dhruv Raj <dhruv.learner@gmail.com>
2023-05-25 08:40:19 +0200

    BLINK analysis

--- analysis_blink.py
+++ analysis_blink.py
@@ -1,13 +1,13 @@
-import blink.main_dense as main_dense
+import BLINK.blink.main_dense as main_dense
 import argparse
-import blink.ner as NER
+import BLINK.blink.ner as NER
 import time
 from datetime import timedelta
 import wikipediaapi
 from urllib.request import urlopen
 import json
 
-models_path = "models/" # the path where you stored the BLINK models
+models_path = "/Users/dhruv/Desktop/NERNEDMP/BLINK/models/" # the path where you stored the BLINK models
 id_from_page_url = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageprops&titles={title}"
 wiki_wiki = wikipediaapi.Wikipedia('en')
 
@@ -62,8 +62,12 @@ print("Starting Analysis ...")
 start_time = time.monotonic()
 
 x = []
-x.append("Bert and Ernie are two Muppets who appear together in numerous skits on the popular children's television show of the United States, Sesame Street.")
+# x.append("Bert and Ernie are two Muppets who appear together in numerous skits on the popular children's television show of the United States, Sesame Street.")
 # x.append("Christopher Columbus was the first man who wanted to find a direct rout to Asia. He stumbled on a new World called America.")
+x.append("His eldest son Akash Ambani, 30, took over as chairman of the board of directors. Ambani, however, will continue to be the chairman of Jio Platforms, the parent company of Reliance Jio Infocomm that owns all Jio digital services brands.")
+x.append("Hermann H. Dieter (* 19. January 1945 in Spaichingen) is a German biochemist and toxicologist who has mainly dealt with the toxicology of drinking water ingredients. Until January 2012, he was head of the department 'Toxicology of drinking and bathing pool water' in the German Federal Environment Agency, before that of the Institute for Water, Soil and Air Hygiene of the Federal Health Office, which was dissolved in 1994.")
+x.append("Michael Douglas Henry 'Mike Kroeger' (* 25. June 1972 in Hanna, Alberta) is a founding member of the Canadian rock band Nickelback, in which he still works as a bassist. Previously, he was already active with some of today's members of Nickelback in the band The Village Idiots.")
+x.append("Sheikh Khalifa bin Hamad Al Thani, the grandfather of the current emir, was born on September 17, 1932. He was the sixth ruler of Qatar and the second ruler of the Al Thani dynasty.")
 
 for text in x:
     print("=" * 100)
@@ -78,13 +82,18 @@ for text in x:
             print(f"Rank: {rank+1}")
             print(f"WP Title: {p}")
             print(f"Score: {round(s, 2)}")
-            page_py = wiki_wiki.page(p)
-            full_url = page_py.fullurl
-            can_url_last = full_url.split("/")[-1]
-            qid = wikidata_id_of_wikipage(can_url_last)
-            print(full_url)
-            print(f"QID: {qid}")
-            print()
+            try:
+                page_py = wiki_wiki.page(p)
+                print(page_py)
+                full_url = page_py.fullurl
+                print(full_url)
+                can_url_last = full_url.split("/")[-1]
+                qid = wikidata_id_of_wikipage(can_url_last)
+                print(full_url)
+                print(f"QID: {qid}")
+                print()
+            except Exception as e:
+                print(f"Error: {str(e)}")
     print("\n")
     print()
 
